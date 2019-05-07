5247983a8eb8b168693e643830f6bc3f1c4e9b23
vytenism <aparance23@gmail.com>
2019-05-07 16:57:57 +0300

    referenses

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 
-$joint1 = false;
-$joint2 = false;
-$joint3 = false;
-$joints2 = &$joint1;
-$joints3 = &$joint2;
-$roll_joints = &$joint3;
-$roll_joints = true;
-print $joint1;
-print $joint2;
-print $joint3;
+$sheep = ['miau'];
+
+for ($x = 0; $x <5; $x++){
+    $sheep []= &$sheep[$x];
+}
+
+var_dump($sheep);
