8f691d601786375522956efeec43fbd677870e9c
vytenism <aparance23@gmail.com>
2019-05-07 17:08:50 +0300

    referenses

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 $sheep = ['miau'];
 
-for ($x = 0; $x <5; $x++){
-    $sheep []= &$sheep[$x];
+for ($x = 0; $x < 5; $x++) {
+    $sheep [] = &$sheep[$x];
 }
 
 var_dump($sheep);
+
+foreach ($sheep as &$value){
+    $value = 'velniop sistema';
+}
+var_dump ($sheep);
