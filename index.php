e8d71d45dc4d3283827a8a55db74ae0fa2e3a79f
vytenism <aparance23@gmail.com>
2019-05-07 16:33:27 +0300

    referenses

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-$x = 0;
-$b = &$x;
-
-unset ($b);
-
-$b = 1;
-
-print $x;
+$joint1 = false;
+$joint2 = false;
+$joint3 = false;
+$joints2 = &$joint1;
+$joints3 = &$joint2;
+$roll_joints = &$joint3;
+$roll_joints = true;
+print $joint1;
+print $joint2;
+print $joint3;
