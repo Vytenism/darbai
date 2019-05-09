18969aaa951f39a77a04cc9bb5d8859dc140bc8a
vytenism <aparance23@gmail.com>
2019-05-09 13:59:42 +0300

    forms 0.3

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
-<?php
+<?php 
 
-
-if (!empty($_POST)){
+if (!empty ($_POST)){
+    $skaicius = ++$_POST['action'];
     
-    
-    $skaicius = $_POST['action'];
-    $skaicius++;
-    
-} else {
-
+}else {
     $skaicius = 0;
+}
+$jpg = [];
+for ($i = 0; $i < $skaicius; $i++){
+    $jpg[$i] = 'banana.jpg';
 }
 
 ?>
 <html>
     <head>
-        <title>links</title>
+        <title>forms</title>
     </head>
     <body>
-        <form method="POST">
-            <button name="action" value="<?php print $skaicius ; ?>"><?php print $skaicius; ?></button>
+        <form method="post">
+            <button name="action" value="<?php print $skaicius; ?>"><?php print $skaicius; ?></button>
         </form>
-        
+        <?php foreach ($jpg as $img): ?>
+        <img src="<?php print $img; ?>">
+        <?php endforeach; ?>
     </body>
 </html>
-
