3e3c7fe0a6bdfe7001da8eef927c5e65a189d591
vytenism <aparance23@gmail.com>
2019-05-14 17:01:18 +0300

    bloku webas

--- index.php
+++ index.php
@@ -1,96 +1,72 @@
 <html>
 <head>
     <title>maxima</title>
-    <link rel="stylesheet" href="includes/normalise.css.css"/>
+    <link rel="stylesheet" href="includes/normalise.css"/>
     <link rel="stylesheet" href="includes/grid.css"/>
     <link rel="stylesheet" href="includes/style.css"/>
     <meta charset="UTF-8">
 </head>
 <body>
-<section class="centered-div">
-    <img class="img1" src="images/maxima_logo.png" alt="maxima">
-    <h1>
-        Maxima Group Puslapis
-    </h1>
-    <a class="nav" href="#apie_mus">Apie Mus</a>
-    <a class="nav" href="#produktai">Produktai</a>
-    <a class="nav" href="#kontaktai">Kontaktai</a>
+<h1>
+    HTML revision
+</h1>
+<h2>
+    Centering HTML Elemnts
+</h2>
+<section id="red">
+    <div id="center">
+        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
+    </div>
 </section>
 <section>
-    <h1 id="apie_mus">
-        Apie Mus
+    <h1 class="text">
+        Text Aligment
     </h1>
-    <img class="img2" src="http://www.maximagrupe.eu/uploads/images/2017/career/img-7.png" alt="logo">
-    <p class="text">Didžiausias Baltijos šalių mažmeninės prekybos tinklo istorija pradėta 1992 metais, Vilniuje
-        atidarius kelias
-        parduotuves su skirtingais pavadinimais. Šiandien prekybos tinklo MAXIMA kūrimosi istoriją galima drąsiai
-        vadinti verslo sėkmės istorija, nes po Nepriklausomybės paskelbimo Baltijos šalyse nė viena įmonė nesulaukė
-        tokios finansinės sėkmės, žinomumo visuomenėje ir pirkėjų įvertinimo.</p>
-
-    <p class="text">Mūsų siekis - kad MAXIMA taptų pirkėjams patogiausia parduotuve, todėl patogumui skiriame didžiausią
-        dėmesį.
-        Patogumas mums reiškia tai, kad klientas parduotuvėse randa prekes išdėstytas aiškiai, tvarkingai ir visur
-        vienodai. Pirkėjui patogu, kai kainos ir kokybės santykis nekelia abejonių, todėl galima pasitikint viską pirkti
-        vienoje vietoje.</p>
+    <div id="left">Hello</div>
+    <div id="middle">Hello</div>
+    <div id="right">Hello</div>
 </section>
 <section>
-    <h1 id="produktai">
-        Produktai
+    <h1 class="text">
+        Box Model
     </h1>
-    <h2>
-        Mesos Gaminiai
-    </h2>
-    <img class="img3" src="http://www.buceris.lt/wp-content/uploads/2016/01/Burger-patties-600x500.jpg" alt="logo">
-    <h3>
-        Jautienos patis
-    </h3>
-    <p>
-        Kaina:€10
-    </p>
-    <img class="img3" src="https://diethood.com/wp-content/uploads/2018/11/Oven-Baked-Chicken-24.jpg" alt="logo">
-    <h3>
-        Vistienos Slaunele
-    </h3>
-    <p>
-        Kaina: €15
-    </p>
-    <h2>
-        Pieno Produktai
-    </h2>
-    <img class="img3" src="http://pienozvaigzdes.lt/194-large_default/dvaro-pienas-25-riebumo-1l.jpg" alt="logo">
-    <h3>
-        Pienas
-    </h3>
-    <p>
-        Kaina: €2
-    </p>
-    <img class="img3" src="http://pienozvaigzdes.lt/resized/705/736/img/p/5/6/56.jpg" alt="logo">
-    <h3>
-        Grudeta Varske
-    </h3>
-    <p>
-        Kaina: €3
-    </p>
+    <div class="red1">
+        <div class="blue">
+            <div class="green">
+                Hello
+            </div>
+        </div>
+    </div>
+    <div class="red2">
+        <div class="blue1">
+            <div class="green">
+                Bye
+            </div>
+        </div>
+    </div>
 </section>
 <section>
-    <h1 id="kontaktai">
-        Kontaktai
+    <h1 class="text">
+        Playing with Inline Box Model
     </h1>
-    <ul>
-        <li>
-            Maxima Group
-        </li>
-        <li>
-            +37060010000
-        </li>
-        <li>
-            mail@maxima.lt
-        </li>
-    </ul>
-    <img class="img4" src="https://www.maxima.lt/custom_assets/appsimoka/images-new/animations/screen-3-content.jpg" alt="logo">
+    <div class="brd">
+        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
+        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
+        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
+    </div>
+    <div class="brd">
+        <a class="algn1" href="">Link3</a>
+        <a class="algn" href="">Link1</a>
+        <a class="algn" href="">Link2</a>
+    </div>
 </section>
-<footer>
-    Copyright © Maxima 2019. All rights reserved.
-</footer>
+<section>
+    <h1 class="text">
+        Playing with Block Margin
+    </h1>
+    <div class="red3"></div>
+    <div class="red3"></div>
+    <div class="red3"></div>
+</section>
 </body>
 </html>
