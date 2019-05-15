aeaa2772b1e7c512541e6ad6e099778b33c86129
vytenism <aparance23@gmail.com>
2019-05-15 16:47:16 +0300

    html form

--- index.php
+++ index.php
@@ -1,72 +1,27 @@
 <html>
 <head>
-    <title>maxima</title>
+    <title>Form</title>
     <link rel="stylesheet" href="includes/normalise.css"/>
-    <link rel="stylesheet" href="includes/grid.css"/>
     <link rel="stylesheet" href="includes/style.css"/>
     <meta charset="UTF-8">
 </head>
 <body>
-<h1>
-    HTML revision
-</h1>
-<h2>
-    Centering HTML Elemnts
-</h2>
-<section id="red">
-    <div id="center">
-        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
-    </div>
-</section>
 <section>
-    <h1 class="text">
-        Text Aligment
-    </h1>
-    <div id="left">Hello</div>
-    <div id="middle">Hello</div>
-    <div id="right">Hello</div>
-</section>
-<section>
-    <h1 class="text">
-        Box Model
-    </h1>
-    <div class="red1">
-        <div class="blue">
-            <div class="green">
-                Hello
-            </div>
-        </div>
+    <div>
+        <h1>
+            Contact Us!
+        </h1>
+        <form>
+            <label for="nm">Name:</label>
+            <input type="text" value="Petras" id="nm">
+            <label for="nm2">Email:</label>
+            <input type="email" value="petras@petras.lt" id="nm2">
+            <label for="nm3">Reference Number:</label>
+            <input type="number" value="12213" id="nm3">
+            <textarea placeholder="Your message goes here..."></textarea>
+            <button>Send!</button>
+        </form>
     </div>
-    <div class="red2">
-        <div class="blue1">
-            <div class="green">
-                Bye
-            </div>
-        </div>
-    </div>
-</section>
-<section>
-    <h1 class="text">
-        Playing with Inline Box Model
-    </h1>
-    <div class="brd">
-        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
-        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
-        <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="logo">
-    </div>
-    <div class="brd">
-        <a class="algn1" href="">Link3</a>
-        <a class="algn" href="">Link1</a>
-        <a class="algn" href="">Link2</a>
-    </div>
-</section>
-<section>
-    <h1 class="text">
-        Playing with Block Margin
-    </h1>
-    <div class="red3"></div>
-    <div class="red3"></div>
-    <div class="red3"></div>
 </section>
 </body>
 </html>
