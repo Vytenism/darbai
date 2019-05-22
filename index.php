86e8a230ee5c4149bf742f837f45efa606459d67
vytenism <aparance23@gmail.com>
2019-05-22 09:42:58 +0300

    Yamaha web

--- index.php
+++ index.php
@@ -1,94 +1,85 @@
 <html>
 <head>
-    <title>Uzduotis</title>
+    <title>Yamaha</title>
     <link rel="stylesheet" type="text/css" href="includes/normalise.css">
     <link rel="stylesheet" type="text/css" href="includes/style.css">
 </head>
 <body>
-<section>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contacts</li>
-    </ul>
-</section>
-<section>
-    <ol>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contacts</li>
-    </ol>
-</section>
-<section class="ops">
-    <p>This paragraph is red</p>
-    <p>This paragraph is blue</p>
-    <p>This paragraph is green</p>
-</section>
-<footer>
-    <p>This paragraph is red</p>
-    <p>This paragraph is blue</p>
-    <p>This paragraph is green</p>
-</footer>
-<header class="head">
-    <div>
-        <img class="imga" src="https://seomofo-da30.kxcdn.com/wp-content/uploads/2018/09/new-google-logo-knockoff1.png" alt="logo">
+<header>
+    <div class="wraper flexone">
+        <img class="imga" src="images/yamaha.png" alt="logo">
         <nav class="navi">
-            <a href="home.php">Home</a>
-            <a href="">About</a>
-            <a href="">Contacts</a>
+            <a href="">Link 1</a>
+            <a href="">Link 2</a>
+            <a href="">Link 3</a>
+            <a href="">Link 4</a>
         </nav>
     </div>
 </header>
-    <div class="container">
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
-        <article>
-            <h2>
-                Title
-            </h2>
-            <p>UGuu asdas sdfdsf sfg sdf sdfg ssdf</p>
-        </article>
+<section>
+    <img class="imgb" src="images/bike-rider.jpg" alt="logo">
+    <div class="relative wraper">
+        <h1 class="absolute">Yamaha Motorcycles</h1>
     </div>
-
+</section>
+<main>
+    <section class="wraper">
+        <h2>Take a look <span>at out work</span></h2>
+        <div class="flextwo">
+            <div class="borders">
+                <img class="imgc" src="images/product.png" alt="logo">
+                <h3>Yamaha Dirt-Lion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas risus nunc, efficitur eget tortor
+                    nec,
+                    fermentum tincidunt justo. Ut vitae arcu viverra, ultrices nunc sed, laoreet orci. Ut a ullamcorper
+                    quam. Pellentesque aliquam lacus tortor, in cursus purus porttitor et. Praesent sed leo diam.</p>
+            </div>
+            <div class="borders">
+                <img class="imgc" src="images/product.png" alt="logo">
+                <h3>Yamaha Dirt-Lion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas risus nunc, efficitur eget tortor
+                    nec,
+                    fermentum tincidunt justo. Ut vitae arcu viverra, ultrices nunc sed, laoreet orci. Ut a ullamcorper
+                    quam. Pellentesque aliquam lacus tortor, in cursus purus porttitor et. Praesent sed leo diam.</p>
+            </div>
+            <div class="borders">
+                <img class="imgc" src="images/product.png" alt="logo">
+                <h3>Yamaha Dirt-Lion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas risus nunc, efficitur eget tortor
+                    nec,
+                    fermentum tincidunt justo. Ut vitae arcu viverra, ultrices nunc sed, laoreet orci. Ut a ullamcorper
+                    quam. Pellentesque aliquam lacus tortor, in cursus purus porttitor et. Praesent sed leo diam.</p>
+            </div>
+            <div class="borders">
+                <img class="imgc" src="images/product.png" alt="logo">
+                <h3>Yamaha Dirt-Lion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas risus nunc, efficitur eget tortor
+                    nec,
+                    fermentum tincidunt justo. Ut vitae arcu viverra, ultrices nunc sed, laoreet orci. Ut a ullamcorper
+                    quam. Pellentesque aliquam lacus tortor, in cursus purus porttitor et. Praesent sed leo diam.</p>
+            </div>
+        </div>
+    </section>
+    <section class="wraper">
+        <h2>Contact us or <span>find more about us</span></h2>
+        <div class="flexthree">
+            <div class="flx">
+                <form>
+                    <input type="text" value="First Name">
+                    <input type="text" value="Last Name">
+                    <input type="email" value="Email">
+                    <textarea type="text">Your message...</textarea>
+                    <button>Send Message</button>
+                </form>
+            </div>
+            <div class="flx">
+                <img class="imgd" src="images/map.png" alt="logo">
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+    Copyright © Petras Slekys 2019. All rights reserved.
+</footer>
 </body>
-</html>
+</html>+
