d9161caab9cc00df0ee6dd56fe6bb3bb28b251a4
vytenism <aparance23@gmail.com>
2019-05-27 14:02:48 +0300

    array with kebabs foreach and for representing

--- index.php
+++ index.php
@@ -1,32 +1,45 @@
 <?php
 
 $pasiula = [
-    [
+    'kebabines' => [
         'Jammi',
         'Wraperia Suppa Kebs',
         'Sinano kebabai',
     ],
-    [
+    'kebabo_tipas' => [
         'kebabas_lavase',
         'kebabas_pitoje',
         'kebabas_leksteje',
     ],
-    [
+    'dydis' => [
         'didelis',
         'mazas',
         'kovotojo',
     ],
-    [
+    'padazas' => [
         'cesnakinis',
         'svelnus',
         'cipotle',
         'astrus',
     ],
-    [
+    'mesa' => [
         'jautiena',
         'vistiena',
     ],
 ];
+$kebabines = '';
+$kebabo_tipas = '';
+$dydis = '';
+$padazas = '';
+$mesa = '';
+
+if (isset($_POST['submit'])) {
+    $kebabines = $_POST['kebabines'];
+    $kebabo_tipas = $_POST['kebabo_tipas'];
+    $dydis = $_POST['dydis'];
+    $padazas = $_POST['padazas'];
+    $mesa = $_POST['mesa'];
+}
 
 ?>
 <!doctype html>
@@ -39,15 +52,23 @@ $pasiula = [
 </head>
 <body>
 <form method="post">
-    <?php for ($item = 0; $item <= count($pasiula) - 1; $item++): ?>
-    <select name="kebabine">
-        <?php foreach ($pasiula[$item] as $key => $kebabine): ?>
-            <option value="<?php print $key; ?>">
-                <?php print $kebabine; ?>
-            </option>
-        <?php endforeach; ?>
-    </select>
-    <?php endfor; ?>
+    <?php foreach ($pasiula as $key => $pasirinkimai): ?>
+        <select name="<?php print $key; ?>">
+            <?php foreach ($pasirinkimai as $key => $kebabine): ?>
+                <option value="<?php print $key; ?>">
+                    <?php print $kebabine; ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+    <?php endforeach; ?>
+    <button type="submit" name="submit">Uzsisakyk!</button>
 </form>
+<section>
+    <p><?php print $kebabines; ?></p>
+    <p><?php print $kebabo_tipas; ?></p>
+    <p><?php print $dydis; ?></p>
+    <p><?php print $padazas; ?></p>
+    <p><?php print $mesa; ?></p>
+</section>
 </body>
 </html>
