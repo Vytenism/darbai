0b02e38439df0340797797cab9250d66ad168b50
vytenism <aparance23@gmail.com>
2019-05-27 16:21:22 +0300

    responsive web about microsoft

--- index.php
+++ index.php
@@ -1,74 +1,83 @@
-<?php
-
-$pasiula = [
-    'kebabines' => [
-        'Jammi',
-        'Wraperia Suppa Kebs',
-        'Sinano kebabai',
-    ],
-    'kebabo_tipas' => [
-        'kebabas_lavase',
-        'kebabas_pitoje',
-        'kebabas_leksteje',
-    ],
-    'dydis' => [
-        'didelis',
-        'mazas',
-        'kovotojo',
-    ],
-    'padazas' => [
-        'cesnakinis',
-        'svelnus',
-        'cipotle',
-        'astrus',
-    ],
-    'mesa' => [
-        'jautiena',
-        'vistiena',
-    ],
-];
-$kebabines = '';
-$kebabo_tipas = '';
-$dydis = '';
-$padazas = '';
-$mesa = '';
-
-if (isset($_POST['submit'])) {
-    $kebabines = $_POST['kebabines'];
-    $kebabo_tipas = $_POST['kebabo_tipas'];
-    $dydis = $_POST['dydis'];
-    $padazas = $_POST['padazas'];
-    $mesa = $_POST['mesa'];
-}
-
-?>
-<!doctype html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="includes/normalise.css">
     <link rel="stylesheet" href="includes/style.css">
-    <title>Array about kebabs</title>
+    <title>Responsive</title>
 </head>
 <body>
-<form method="post">
-    <?php foreach ($pasiula as $key => $pasirinkimai): ?>
-        <select name="<?php print $key; ?>">
-            <?php foreach ($pasirinkimai as $key => $kebabine): ?>
-                <option value="<?php print $key; ?>">
-                    <?php print $kebabine; ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-    <?php endforeach; ?>
-    <button type="submit" name="submit">Uzsisakyk!</button>
-</form>
-<section>
-    <p><?php print $kebabines; ?></p>
-    <p><?php print $kebabo_tipas; ?></p>
-    <p><?php print $dydis; ?></p>
-    <p><?php print $padazas; ?></p>
-    <p><?php print $mesa; ?></p>
-</section>
+<header>
+    <div class="content">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/2000px-Microsoft_logo_%282012%29.svg.png"
+             alt="logo">
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#about">About Us</a>
+            <a href="#services">Services</a>
+            <a href="#contacts">Contacts</a>
+        </nav>
+    </div>
+</header>
+<main>
+    <section id="home">
+        <div class="content-section">
+            <h1><span>Hello</span></h1>
+            <div>
+                <a href="#">Find out more about us!</a>
+            </div>
+        </div>
+    </section>
+    <section id="about">
+        <div class="content-section">
+            <h1><span>About Us</span></h1>
+            <p>
+                Sed auctor diam neque, ut blandit nisi accumsan eget. Nullam finibus commodo risus, vel auctor urna
+                tincidunt
+                eu. Integer sollicitudin lectus eget nisi sagittis, et blandit nisl consectetur. Nunc ultricies velit
+                non
+                auctor euismod. Fusce iaculis id felis ac blandit. Suspendisse sit amet finibus nisi, in ullamcorper
+                justo.
+                Sed efficitur lectus ac mattis bibendum. Aliquam accumsan nulla id nunc facilisis rhoncus. Sed vulputate
+                nulla vitae tincidunt feugiat. Morbi at rutrum velit, et congue massa.
+            </p>
+        </div>
+    </section>
+    <section id="services">
+        <div class="content-section">
+            <h1><span>Services</span></h1>
+            <p>
+                Pellentesque viverra, risus et posuere faucibus, eros neque scelerisque lacus, vitae tempus purus dolor
+                in
+                turpis. Fusce varius consectetur commodo. Proin sem nisi, posuere non lobortis cursus, dignissim in
+                diam.
+                Nunc ipsum magna, lacinia ac purus a, facilisis sollicitudin turpis. Pellentesque hendrerit justo ex,
+                quis
+                lacinia diam sollicitudin sed. Integer tempus dolor non commodo maximus. Pellentesque ut orci
+                pellentesque
+                lacus rutrum convallis. Ut rhoncus mi tortor, et malesuada quam hendrerit et. Vivamus a diam augue.
+                Quisque
+                id sollicitudin nibh. Nam eleifend sollicitudin ipsum, ut egestas tellus laoreet et. Phasellus vel felis
+                mollis, luctus orci eu, placerat eros. Nam gravida tellus id ligula interdum scelerisque. Quisque lacus
+                lorem, semper sed orci a, dapibus lobortis sem.
+            </p>
+        </div>
+    </section>
+    <section id="contacts">
+        <div class="content-section">
+            <h1>
+                <span>Contacts</span>
+            </h1>
+            <ol>
+                <li>+370675754401</li>
+                <li>Email@email.com</li>
+                <li>CodeAcademy, Vilnius</li>
+            </ol>
+        </div>
+    </section>
+</main>
+<footer>
+    Copyright © Microsoft 2018. All rights reserved. <a href="#">Go UP!</a>
+</footer>
 </body>
 </html>
