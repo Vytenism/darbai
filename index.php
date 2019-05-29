be18a74e281417c90248ab5cda2344a846277d9f
vytenism <aparance23@gmail.com>
2019-05-29 14:29:25 +0300

    responsive web microsoft forms and etc

--- index.php
+++ index.php
@@ -1,87 +1,215 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$konsultacija = '';
-$paskaita = '';
-
-
-if (isset($_POST['submit'])) {
-    $vardas = $_POST['name'];
-    $pavarde = $_POST['lname'];
-    $kalendorius = $_POST['date'];
-
-    if (!isset($_POST['konsultacijos'])) {
-        $konsultacija = 'Nebuvo';
-    } else {
-        $konsultacija = 'Buvo';
-    }
-
-    if (!isset($_POST['paskaitos'])) {
-        $paskaita = 'Nebuvo';
-    } else {
-        $paskaita = 'Buvo';
-    }
-
-} else {
-    $vardas = 'Nepateikta';
-    $pavarde = 'Nepateikta';
-    $kalendorius = 'Nepateikta';
-
-}
-//DUOMENU IRASYMAS I .TXT FAILA
-if  (isset($_POST['submit'])) {
-    //    SUKURIAMAS KINTAMASIS APJUNGIANTIS VISUS POST LAUKELIUS
-    $data = $_POST['name'] . $_POST['lname'] . $_POST['date'] . $_POST['konsultacijos'] . $_POST['paskaitos'];
-    //    NURODOMA .TXT FAILO VIETA IR KAS BUS JAME DAROMA
-    $fp = fopen('info.txt', 'a');
-    //    IRASOMI DUOMENYS I NURODYTA .TXT FAILA
-    fwrite($fp, $data);
-    //    UZDAROMAS FAILAS
-    fclose($fp);
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Klases lankomumas</title>
+    <title>Form</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="includes/style.css">
+    <link rel="stylesheet" href="includes/normalise.css">
+    <link rel="stylesheet" href="includes/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 <body>
+<header class="background center">
+    <div>
+        <img class="imga" src="images/microsoftwhite.png" alt="logo">
+    </div>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+</header>
+<main>
+    <section class="background retract">
+        <div class="wrapper height">
+            <h1 class="border">
+                Hello
+            </h1>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 retract">
+        <div class="wrapper height">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 flex">
+        <div class="imgb flex3">
 
-<form method="POST">
-    <label for="name">Name:</label>
-    <input type="text" name="name">
-    <label for="lname">Last name:</label>
-    <input type="text" name="lname">
-    <label for="date">Date:</label>
-    <input type="date" name="date">
-    <label for="kons">Kons:</label>
-    <input type="checkbox" name="konsultacijos">
-    <label for="pask">Pask:</label>
-    <input type="checkbox" name="paskaitos">
-    <input type="submit" value="Submit" name="submit">
-</form>
+        </div>
+        <div class="retract flex2">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 flex">
+        <div class="imgb flex3">
 
-<table>
-    <tr>
-        <th>Name:</th>
-        <th>Last name:</th>
-        <th>Date:</th>
-        <th>Kons:</th>
-        <th>Pask:</th>
-    </tr>
-    <tr>
-        <td><?php print $vardas; ?></td>
-        <td><?php print $pavarde; ?></td>
-        <td><?php print $kalendorius; ?></td>
-        <td><?php print $konsultacija; ?></td>
-        <td><?php print $paskaita; ?></td>
-    </tr>
-</table>
+        </div>
+        <div class="retract flex2">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 flex">
+        <div class="imgb flex3">
 
+        </div>
+        <div class="retract flex2">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 flex">
+        <div class="imgb flex3">
+
+        </div>
+        <div class="retract flex2">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2 flex">
+        <div class="imgb flex3">
+
+        </div>
+        <div class="retract flex2">
+            <h2 class="border2">
+                Hello
+            </h2>
+            <p>
+                This can be a lot of text
+            </p>
+        </div>
+    </section>
+    <section class="background2">
+        <form method="post" class="retract wrapper">
+            <div>
+                <h2 class="border2">
+                    Add Employee
+                </h2>
+            </div>
+            <label for="name">NAME:</label>
+            <input class="inputconfig" type="text" id="name" name="name" placeholder="Vytenis">
+            <label for="email">EMAIL:</label>
+            <input class="inputconfig" type="email" id="email" name="email" placeholder="aparance@gmail.com">
+            <label for="phone">PHONE:</label>
+            <input class="inputconfig" type="number" id="phone" name="phone" placeholder="862020619">
+            <label for="code">4 + 3</label>
+            <input class="inputconfig" type="number" id="code" name="calculation">
+            <button type="submit" name="submit">ADD EMPLOYEE</button>
+        </form>
+    </section>
+    <section class="background2">
+        <div class="retract wrapper">
+            <table>
+                <h2 class="border2">
+                    Employee Table
+                </h2>
+                <tr>
+                    <th>Vardas</th>
+                    <th>El. paštas</th>
+                    <th>Telefonas</th>
+                </tr>
+                <tr>
+                    <td>Petras</td>
+                    <td>petras@mozilla.lt</td>
+                    <td>867574401</td>
+                </tr>
+                <tr>
+                    <td>Petras</td>
+                    <td>petras@mozilla.lt</td>
+                    <td>867574401</td>
+                </tr>
+                <tr>
+                    <td>Petras</td>
+                    <td>petras@mozilla.lt</td>
+                    <td>867574401</td>
+                </tr>
+                <tr>
+                    <td>Petras</td>
+                    <td>petras@mozilla.lt</td>
+                    <td>867574401</td>
+                </tr>
+            </table>
+        </div>
+    </section>
+    <section class="background2">
+        <div class="flex4 wrapper push">
+            <div class="flex5">
+                <form class="retract2">
+                    <label for="name2">Full name:</label>
+                    <input class="inputconfig" type="text" id="name2" name="name" placeholder="Vytenis">
+                    <label for="email2">Email:</label>
+                    <input class="inputconfig" type="email" id="email2" name="email" placeholder="aparance@gmail.com">
+                    <label for="message">Message:</label>
+                    <textarea id="message" placeholder="Your message..."></textarea>
+                    <input class="bottun2" type="submit" name="submit" value="send!">
+                    <input class="bottun2" type="reset" name="reset" value="cleaer">
+                </form>
+            </div>
+            <div class="retract2 flex5">
+                <div class="sizing">
+                    <div class="inline2 icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <div class="inline">
+                        <h3>
+                            Phone
+                        </h3>
+                        37067574401
+                    </div>
+                </div>
+                <div class="sizing">
+                    <div class="inline2 icon">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="inline">
+                        <h3>
+                            Address
+                        </h3>
+                        info@info.lt
+                    </div>
+                </div>
+                <div class="sizing">
+                    <div class="inline2 icon">
+                        <i class="fas fa-home"></i>
+                    </div>
+                    <div class="inline">
+                        <h3>
+                            Address
+                        </h3>
+                        Mindaugo str. 16
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+    Copyright © Petras Slekys
+</footer>
 </body>
 </html>
