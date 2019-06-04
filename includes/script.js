d1477f3035918c05cda28a10622783f6c6db46e9
vytenism <aparance23@gmail.com>
2019-06-04 14:46:45 +0300

    function about army you need to go or not

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,14 @@
-var button = document.querySelector("input[type=submit]");
-button.addEventListener("click", names);
-
-function names(){
-    var inpt = document.querySelector("input[type=text]");
-    button.value = inpt.value;
+let button = document.querySelector("input[type=submit]");
+button.addEventListener("click", count);
+function count(){
+       var number = document.querySelector("input[type=text]").value;
+       var radio = document.querySelector("input[type=radio]:checked");
+       console.log(radio);
+       if (number >= 18 && number <= 30 && radio === "teistas"){
+            document.querySelector("p:nth-of-type(3)").innerHTML = "Tinki i kariuomene nes esi neteistas"; 
+            alert("i kariuomene");      
+       } else { 
+           document.querySelector("p:nth-of-type(3)").innerHTML = "netinki nes esi teistas";
+           alert("nereikia");
+       }
 }
