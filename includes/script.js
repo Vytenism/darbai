d4b07d0d21a5162c3f70f488fcb47002e3d4d4b4
vytenism <aparance23@gmail.com>
2019-06-04 15:29:33 +0300

    while for function

--- includes/script.js
+++ includes/script.js
@@ -1,14 +1,20 @@
-let button = document.querySelector("input[type=submit]");
-button.addEventListener("click", count);
-function count(){
-       var number = document.querySelector("input[type=text]").value;
-       var radio = document.querySelector("input[type=radio]:checked");
-       console.log(radio);
-       if (number >= 18 && number <= 30 && radio === "teistas"){
-            document.querySelector("p:nth-of-type(3)").innerHTML = "Tinki i kariuomene nes esi neteistas"; 
-            alert("i kariuomene");      
-       } else { 
-           document.querySelector("p:nth-of-type(3)").innerHTML = "netinki nes esi teistas";
-           alert("nereikia");
-       }
+'use strict';
+var sub = document.querySelector("input[type=submit]");
+sub.addEventListener("click", myname2);
+function myname(){
+    var fname = document.querySelector("input[type=number]").value;
+    var age = 0;
+    while(age < fname){
+        console.log("Vyte");
+        age++;
+    }
+    myname();
+}
+
+function myname2(){
+    var fnamea = document.querySelector("input[type=number]").value;
+    var ageb = 0;
+    for (ageb = 0; ageb <fnamea; ageb++){
+        console.log("Petras");
+    }
 }
