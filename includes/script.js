716e54fb98be689a5728baa7ca34cabdd5f566b2
vytenism <aparance23@gmail.com>
2019-06-05 11:10:47 +0300

    homework 06.05 about christmas gift and guess loto

--- includes/script.js
+++ includes/script.js
@@ -1,15 +1,27 @@
 'use strict';
 let button = document.querySelector("input[type=submit]");
-button.addEventListener("click", abc);
+button.addEventListener("click", ab);
 
-function abc(){
-    var sk = document.getElementById("x");
-    sk = Number(sk.value);
-    for(let i = 0; i < sk; i++){
-        for(let o = 0; o <= i; o++){
-             document.write("*");
+
+    let age = prompt("irasykite savo darbo stazo metus");
+    if (0 <= age && age <= 10){
+        document.querySelector("h1").innerHTML = "Jums priklauso 50 pound dovana";
+    } else if (10 < age && age <= 20){
+        document.querySelector("h1").innerHTML = "Jums priklauso 50 pound + 50 puond dovana uz ilga bendradarbiavima";
+    } else if(age >= 21){
+        document.querySelector("h1").innerHTML = "Jus esat musu Herojus jus gaunate 50 pound + 100 pound dovanu uz geras akimirkas kartu";
+    } else {
+        document.querySelector("h1").innerHTML = "Jus nedirbate kartu su mumis todel gaunate spyga taukuota :)";
+    }
+function ab(){
+    let num = 10;
+    let guess;
+    do{
+        guess = prompt("atspek skaiciu");
+        if (guess !== num){
+            alert("bandyk dar karta");
         }
-        document.write("<br>");
-    }       
-}
+    } while(guess != num);
 
+    alert("atspejai");
+}
