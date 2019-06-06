7588cb1258708b41647e15d18412c592d6b09d58
vytenism <aparance23@gmail.com>
2019-06-06 11:46:47 +0300

    triangle pyramid revpyramid and clear allmethod

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,24 @@
 'use strict';
+var triangle = document.getElementById("triangle");
 var pyramid = document.getElementById("piramid");
-pyramid.addEventListener("click", abc);
-function abc(){
+var revpyramid = document.getElementById("revpiramid");
+var clear = document.getElementById("clear");
+triangle.addEventListener("click", trianglea);
+pyramid.addEventListener("click", pyramida);
+revpyramid.addEventListener("click", reverse);
+clear.addEventListener("click", cleara);
+
+function trianglea(){
+    var num = 7;
+    for (var i = 0; i <= num; i++){
+        for (var j = 0; j < i; j++){
+            document.querySelector(".app").innerHTML += "*";
+        }
+        document.querySelector(".app").innerHTML += "<br>";
+    }
+}
+
+function pyramida(){
     var num = 7;
     for (var i = 0; i < num; i++){
         for (var j = (2*num - i); j >= i; j--){
@@ -12,4 +29,21 @@ function abc(){
         }
         document.querySelector(".app").innerHTML += "<br>";
     }
+}
+
+function reverse(){
+    var num = 17;
+    for (var i = 0; i <= num; i++){
+        for (var j = 0; j <= (2*i + 1); j++){
+            document.querySelector(".app").innerHTML += "&nbsp;";
+        }
+        for (var o = 1; o <= (num - 2*i); o++){
+            document.querySelector(".app").innerHTML += "*";
+        }
+        document.querySelector(".app").innerHTML += "<br>";     
+    }
+}
+
+function cleara(){
+    document.querySelector(".app").innerHTML = "";
 }
