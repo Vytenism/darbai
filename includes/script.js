d982aa0471695b32d9857fea59e23f3ca09ecd49
vytenism <aparance23@gmail.com>
2019-06-06 10:40:32 +0300

    pyramid 2nd method

--- includes/script.js
+++ includes/script.js
@@ -1,33 +1,15 @@
 'use strict';
-let button = document.getElementById("triangle");
-let button2 = document.getElementById("piramid");
-// let button3 = document.getElementById("revpiramid");
-// let button4 = document.getElementById("clear");
-
-button.addEventListener("click", triangle);
-button2.addEventListener("click", piramid);
-
-// button3.addEventListener("click", triangle);
-// button4.addEventListener("click", piramid);
-function triangle(){
+var pyramid = document.getElementById("piramid");
+pyramid.addEventListener("click", abc);
+function abc(){
     var num = 7;
-    for (var i = 0; i <= num; i++){
-        for (var j = 0; j < i; j++){
-            document.write("*");
+    for (var i = 0; i < num; i++){
+        for (var j = (2*num - i); j >= i; j--){
+            document.querySelector(".app").innerHTML += "&nbsp;";
         }
-        document.write("<br>");
-    }
-}
-
-function piramid(){
-    var numb = 70;
-    for (var i = 0; i < numb; i++){
-        for (let a = (2*numb - i); a >= i; a--){
-            document.write("&nbsp;");
+        for (var o = 1; o < 2*i; o++){
+            document.querySelector(".app").innerHTML += "*";
         }
-        for (var j = 1; j < 2*i; j++){
-            document.write("*");
-        }
-        document.write("<br>");
+        document.querySelector(".app").innerHTML += "<br>";
     }
 }
